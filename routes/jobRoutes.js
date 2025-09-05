a8ed7303a6ea3e0d09339bb3dadee2c56391f03a
RastkoD <raleliverpool@gmail.com>
2025-09-05 17:19:59 +0200

    add simple auth

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -7,6 +7,14 @@ const {
 const express = require("express");
 const router = express.Router();
 
+const auth = (req, res, next) => {
+  const password = req.headers["x-app-password"];
+  if (password !== process.env.APP_PASSWORD) {
+    return res.status(401).json({ error: "Unauthorized access" });
+  }
+  next();
+};
+
 router.get("/", async (req, res) => {
   try {
     const jobs = await getAllJobs();
@@ -16,7 +24,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   try {
     const data = await addJob(req.body);
     res.status(201).json(data);
@@ -25,7 +33,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   try {
     const id = req.params.id;
     await deleteJobById(id);
@@ -35,7 +43,7 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
   try {
     const updatedJob = await updateJob(req.params.id, req.body);
     res.json(updatedJob[0]);
